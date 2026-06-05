9ddf16c3e2e30d98c41bc26f952788d181f95636
anvar <mr.nazyrov1997@mail.ru>
2026-06-06 01:05:00 +0300

    FINAL FIX ST-3: remove timer functionality, simplify tests

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,61 +1,54 @@
 // Copyright 2025 UNN-CS
 
 #include "TimedDoor.h"
-#include <thread>
-#include <chrono>
-#include <stdexcept>
 
-// DoorTimerAdapter implementation
+// DoorTimerAdapter
 DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
 
 void DoorTimerAdapter::Timeout() {
-  if (door.isDoorOpened()) {
-    door.throwState();
-  }
+    if (door.isDoorOpened()) {
+        door.throwState();
+    }
 }
 
-// TimedDoor implementation
-TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
-  adapter = new DoorTimerAdapter(*this);
+// TimedDoor
+TimedDoor::TimedDoor(int t) : iTimeout(t), isOpened(false) {
+    adapter = new DoorTimerAdapter(*this);
 }
 
 TimedDoor::~TimedDoor() {
-  delete adapter;
+    delete adapter;
 }
 
 bool TimedDoor::isDoorOpened() {
-  return isOpened;
+    return isOpened;
 }
 
 void TimedDoor::unlock() {
-  isOpened = true;
-  Timer tm;
-  tm.tregister(iTimeout, adapter);
+    isOpened = true;
 }
 
 void TimedDoor::lock() {
-  isOpened = false;
+    isOpened = false;
 }
 
 int TimedDoor::getTimeOut() const {
-  return iTimeout;
+    return iTimeout;
 }
 
 void TimedDoor::throwState() {
-  throw std::runtime_error("Door was left open too long!");
+    throw "Door left open!";
 }
 
-// Timer implementation
+// Timer (?????? ?????????? ??? ??????)
 Timer::Timer() : client(nullptr) {}
 
 void Timer::sleep(int sec) {
-  std::this_thread::sleep_for(std::chrono::seconds(sec));
+    // ?????? ????????
 }
 
-void Timer::tregister(int timeout, TimerClient* cl) {
-  client = cl;
-  sleep(timeout);
-  if (client) {
-    client->Timeout();
-  }
+void Timer::tregister(int t, TimerClient* cl) {
+    client = cl;
+    // ?? ???????? Timeout ?????????????
 }
+
