20a7199f4fddcfc748f9c7c7fef14b9898bd1c01
anvar <mr.nazyrov1997@mail.ru>
2026-06-05 16:00:39 +0300

    Назыров Анвар 3823б1пр4 st-3 ветка b164

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,58 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2025 UNN-CS
+
 #include "TimedDoor.h"
+#include <thread>
+#include <chrono>
+
+// DoorTimerAdapter
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
+
+void DoorTimerAdapter::Timeout() {
+    if (door.isDoorOpened()) {
+        door.throwState();
+    }
+}
+
+// TimedDoor
+TimedDoor::TimedDoor(int t) : iTimeout(t), isOpened(false) {
+    adapter = new DoorTimerAdapter(*this);
+}
+
+TimedDoor::~TimedDoor() {
+    delete adapter;
+}
+
+bool TimedDoor::isDoorOpened() {
+    return isOpened;
+}
+
+void TimedDoor::unlock() {
+    isOpened = true;
+    Timer tm;
+    tm.tregister(iTimeout, adapter);
+}
+
+void TimedDoor::lock() {
+    isOpened = false;
+}
+
+int TimedDoor::getTimeOut() {
+    return iTimeout;
+}
+
+void TimedDoor::throwState() {
+    throw "Door left open!";
+}
+
+// Timer
+void Timer::sleep(int sec) {
+    std::this_thread::sleep_for(std::chrono::seconds(sec));
+}
+
+void Timer::tregister(int t, TimerClient* cl) {
+    client = cl;
+    sleep(t);
+    if (client) {
+        client->Timeout();
+    }
+}
